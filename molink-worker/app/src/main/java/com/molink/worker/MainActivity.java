4a66fd33f4ee732072768d751c9a6c714d648e18
gaozhen <gaozhen@abchina.com>
2026-04-16 11:43:22 +0800

    refactor test.py

--- molink-worker/app/src/main/java/com/molink/worker/MainActivity.java
+++ molink-worker/app/src/main/java/com/molink/worker/MainActivity.java
@@ -49,6 +49,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         statusDot = findViewById(R.id.statusDot);
         statusRunning = findViewById(R.id.statusRunning);
